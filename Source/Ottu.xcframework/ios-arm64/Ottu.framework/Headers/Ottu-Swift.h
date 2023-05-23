1a8df1cc35feed8b0e169c4781ecf9203e1f8322
Maninder <er.maninderbindra@gmail.com>
2023-05-23 23:27:06 +0530

    updated code

--- Source/Ottu.xcframework/ios-arm64/Ottu.framework/Headers/Ottu-Swift.h
+++ Source/Ottu.xcframework/ios-arm64/Ottu.framework/Headers/Ottu-Swift.h
@@ -260,6 +260,8 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import CoreFoundation;
+@import UIKit;
 #endif
 
 #endif
@@ -280,6 +282,17 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+@class NSCoder;
+
+SWIFT_CLASS("_TtC4Ottu14ApplepayButton")
+@interface ApplepayButton : UIButton
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithFrame:(CGRect)frame OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
+- (void)drawRect:(CGRect)rect;
+- (void)layoutSubviews;
+@end
+
 
 
 
