5f4316a42036ca75ccf1ff52eb0908c36daaf8c6
Maninder <er.maninderbindra@gmail.com>
2023-05-23 21:58:39 +0530

    Apple pay button bug fixing

--- Source/Ottu.xcframework/ios-arm64/Ottu.framework/Headers/Ottu-Swift.h
+++ Source/Ottu.xcframework/ios-arm64/Ottu.framework/Headers/Ottu-Swift.h
@@ -284,10 +284,11 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if defined(__OBJC__)
 @class NSCoder;
 
+IB_DESIGNABLE
 SWIFT_CLASS("_TtC4Ottu14ApplepayButton")
 @interface ApplepayButton : UIButton
-- (nonnull instancetype)initWithFrame:(CGRect)frame SWIFT_UNAVAILABLE;
-- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder SWIFT_UNAVAILABLE;
+- (nonnull instancetype)initWithFrame:(CGRect)frame OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 - (void)drawRect:(CGRect)rect;
 @end
 
