9ad7f890f524b713c0ce5eeddd3ab4089f366d87
Maninder <er.maninderbindra@gmail.com>
2023-05-24 08:28:00 +0530

    Apple storyboard bug fixing

--- Source/Ottu.xcframework/ios-arm64_x86_64-simulator/Ottu.framework/Headers/Ottu-Swift.h
+++ Source/Ottu.xcframework/ios-arm64_x86_64-simulator/Ottu.framework/Headers/Ottu-Swift.h
@@ -260,8 +260,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
-@import CoreFoundation;
-@import UIKit;
 #endif
 
 #endif
@@ -282,17 +280,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
-@class NSCoder;
-
-SWIFT_CLASS("_TtC4Ottu14ApplepayButton")
-@interface ApplepayButton : UIButton
-- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
-- (nonnull instancetype)initWithFrame:(CGRect)frame OBJC_DESIGNATED_INITIALIZER;
-- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
-- (void)drawRect:(CGRect)rect;
-- (void)layoutSubviews;
-@end
-
 
 
 
@@ -570,8 +557,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
-@import CoreFoundation;
-@import UIKit;
 #endif
 
 #endif
@@ -592,17 +577,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
-@class NSCoder;
-
-SWIFT_CLASS("_TtC4Ottu14ApplepayButton")
-@interface ApplepayButton : UIButton
-- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
-- (nonnull instancetype)initWithFrame:(CGRect)frame OBJC_DESIGNATED_INITIALIZER;
-- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
-- (void)drawRect:(CGRect)rect;
-- (void)layoutSubviews;
-@end
-
 
 
 
